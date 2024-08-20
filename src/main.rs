45ab072d3c6f43417821740835b1d6bc65bde261
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-20 15:57:45 +0200

    feat(part1): Basics + Tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // construct a subscriber that prints formatted traces to stdout
+    // let subscriber = tracing_subscriber::FmtSubscriber::new();
+
+    // Start configuring a `fmt` subscriber
+    tracing_subscriber::fmt()
+        // Use a more compact, abbreviated log format
+        .compact()
+        // Don't display the event's target (module path)
+        .with_target(false)
+        // Build the subscriber
+        .init();
+
+    // Build our new Router
+    let app: axum::Router<()> = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
+
+    // Run our application as a hyper server on http://localhost:3001
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
 }
