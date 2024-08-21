0311693dc6b4461640bc2ffdfe0728581a425697
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-21 16:26:28 +0200

    feat: Add Tracing (stdout / file) w/ json ...

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,35 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // construct a subscriber that prints formatted traces to stdout
     // let subscriber = tracing_subscriber::FmtSubscriber::new();
 
-    // Start configuring a `fmt` subscriber
+    // * INFO:  Start configuring a `fmt` subscriber
     tracing_subscriber::fmt()
         // Use a more compact, abbreviated log format
         .compact()
+        //.json()
         // Don't display the event's target (module path)
         .with_target(false)
         // Build the subscriber
         .init();
 
-    // Build our new Router
+    // * INFO:  Start configuring a `Registry` subscriber
+    /*
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    */
+
+    // * INFO:  Start configuring a `appender` subscriber
+    /*
+    let info_file = rolling::daily("./logs", "info");
+
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .with_writer(info_file)
+        .with_ansi(false)
+        .init();
+    */
+
+    // * INFO: Build our new Router
     let app: axum::Router<()> = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .layer(
@@ -25,7 +44,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
         );
 
-    // Run our application as a hyper server on http://localhost:3001
+    // * INFO: Run our application as a hyper server on http://localhost:3001
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
         .unwrap();
